082f05583ae9b8532a8c27c0be9c74848a714a26
Maria Paz <pazrodriguez612@gmail.com>
2022-12-11 19:32:30 -0600

    2D arrays

--- src/Basic/AList.java
+++ src/Basic/AList.java
@@ -6,12 +6,33 @@ public class AList {
     public static void main(String[] args) {
         // ArrayList: resizable Array
 
-        ArrayList<String> food = new ArrayList<>();
-        ArrayList<Integer> nums = new ArrayList<>();
+         // 2D ArrayList
+         ArrayList<String> bakeryList = new ArrayList<>();
+         bakeryList.add("Concha");
+         bakeryList.add("Pastel de tres leches");
+         bakeryList.add("donuts");
+ 
+         ArrayList<String> produceList = new ArrayList<>();
+         produceList.add("Brocoli");
+         produceList.add("Onions");
+         produceList.add("Tomatos");
+         produceList.add("Carrots");
 
+
+        ArrayList<String> food = new ArrayList<>();
         food.add("Enchiladas");
         food.add("Tacos");
         food.add("Tamales");
+
+        ArrayList<ArrayList<String>> groceryList = new ArrayList<>();
+
+        groceryList.add(bakeryList);
+        groceryList.add(produceList);
+        groceryList.add(food);
+
+        System.out.println(groceryList);
+        System.out.println(groceryList.get(1).get(2));
+
 
         for(String f: food) {
             System.out.println(f);
@@ -20,6 +41,8 @@ public class AList {
         for(int i = 0; i < food.size(); i++) {
             System.out.println(food.get(i));
         }
+
+        ArrayList<Integer> nums = new ArrayList<>();
 
         for(int i = 0; i < 20; i++) {
             nums.add(i + i);
@@ -32,5 +55,8 @@ public class AList {
         System.out.println(nums);
         nums.clear();
         System.out.println(nums);
+       
+
+        
     }
 }
