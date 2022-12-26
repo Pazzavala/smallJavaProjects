1b3fe982c4ae2c1a992ff2363cc83c96f60bbb66
Maria Zavala <zaval054@umn.edu>
2022-12-25 23:37:01 -0600

    Done except game loop

--- src/TicTacToeGame/TicTacToe.java
+++ src/TicTacToeGame/TicTacToe.java
@@ -7,18 +7,76 @@ import java.util.Random;
 
 public class TicTacToe extends JFrame implements ActionListener {
     boolean xTurn;
-    JPanel titlePanel;
-    JPanel gamePanel;
-    JButton[] buttons;
-    JLabel title;
+    boolean keepPlaying = true;
+    JButton replay = new JButton("Replay");
+    JButton quit = new JButton("Quit");
+    JPanel gameButtonsPanel = new JPanel();
+    JPanel titlePanel = new JPanel();
+    JPanel gamePanel = new JPanel();
+    JPanel scorePanel = new JPanel();
+    JButton[] buttons = new JButton[9];
+    JLabel title = new JLabel("TicTacToe");
+    int xPoints = 0;
+    int oPoints = 0;
+    JLabel xScore = new JLabel("X: " + xPoints);
+    JLabel oScore = new JLabel("O: " + oPoints);
+
     Random rand = new Random();
 
     TicTacToe() {
-        titlePanel = new JPanel();
-        gamePanel = new JPanel();
-        buttons = new JButton[9];
-        title = new JLabel("TicTacToe");
+        drawBoard();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        if (e.getSource() == replay)
+
+            drawBoard();
+        else if (e.getSource() == quit)
+            System.exit(0);
+
+        for (int i = 0; i < 9; i++) {
+            if (e.getSource() == buttons[i]) {
+                buttons[i].setFont(new Font("Mv Boli", Font.BOLD, 60));
+
+                if (xTurn) {
+                    if (buttons[i].getText().equals("")) {
+                        buttons[i].setForeground(Color.RED);
+                        buttons[i].setText("X");
+                        xTurn = false;
+                        title.setText("O Turn");
+                        checkWins();
+                    }
+                } else {
+                    if (buttons[i].getText().equals("")) {
+                        buttons[i].setForeground(Color.BLUE);
+                        buttons[i].setText("O");
+                        xTurn = true;
+                        title.setText("X Turn");
+                        checkWins();
+                    }
+                }
+            }
+        }
+    }
+
+    public void drawBoard() {
         xTurn = firstTurn();
+
+        gameButtonsPanel.setLayout(new GridLayout());
+
+        replay.setSize(new Dimension(6, 3));
+        quit.setSize(new Dimension(6, 3));
+
+        replay.setFont(new Font("Ink Free", Font.BOLD,  20));
+        quit.setFont(new Font("Ink Free", Font.BOLD,  20));
+
+        replay.addActionListener(this);
+        quit.addActionListener(this);
+
+        gameButtonsPanel.add(replay);
+        gameButtonsPanel.add(quit);
 
         title.setFont(new Font("Ink Free", Font.BOLD,  75));
         title.setForeground(Color.WHITE);
@@ -31,121 +89,124 @@ public class TicTacToe extends JFrame implements ActionListener {
         gamePanel.setPreferredSize(new Dimension(500, 500));
         gamePanel.setBackground(Color.BLACK);
 
+        scorePanel.setLayout(new BorderLayout());
+
+        oScore.setFont(new Font("Ink Free", Font.BOLD,  50));
+        scorePanel.add(xScore, BorderLayout.EAST);
+
+        scorePanel.add(gameButtonsPanel, BorderLayout.CENTER);
+
+        xScore.setFont(new Font("Ink Free", Font.BOLD,  50));
+        scorePanel.add(oScore, BorderLayout.WEST);
+
         for (int i = 0; i < 9; i++) {
             buttons[i] = new JButton();
             buttons[i].addActionListener(this);
             gamePanel.add(buttons[i]);
         }
 
-        this.setSize(new Dimension(500,500));
+        this.setSize(new Dimension(600,600));
         this.setLayout(new BorderLayout());
+
         this.add(titlePanel, BorderLayout.NORTH);
         this.add(gamePanel, BorderLayout.CENTER);
+        this.add(scorePanel, BorderLayout.SOUTH);
 
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setTitle("TicTacToe");
         this.setVisible(true);
     }
 
-//    TODO: Check who's turn it is and depending of who's it is
-//     make the click of button turn either into an x or an o
-    @Override
-    public void actionPerformed(ActionEvent e) {
-
-        for (int i = 0; i < 9; i++) {
-            if (e.getSource() == buttons[i]) {
-                buttons[i].setFont(new Font("Mv Boli", Font.BOLD, 60));
-
-                if (xTurn) {
-                    if (buttons[i].getText() == "") {
-                        buttons[i].setForeground(Color.RED);
-                        buttons[i].setText("X");
-                        xTurn = false;
-                    }
-                } else {
-                    if (buttons[i].getText() == "") {
-                        buttons[i].setForeground(Color.BLUE);
-                        buttons[i].setText("O");
-                        xTurn = true;
-                    }
-                }
-            }
-        }
-    }
-
-//    TODO: Set who's turn it is to play with a random variable
     public boolean firstTurn() {
-
-        int player = rand.nextInt(2);
-
-        if (player == 0) {
-           return true;
+        if(rand.nextInt(2) == 0) {
+            title.setText("X Turn");
+            return true;
         } else {
+            title.setText("O Turn");
             return false;
         }
     }
 
-//    TODO: Check winning combinations for all possible combos for x and o
     public void checkWins() {
-
 //        Horizontal
-        if (buttons[0].getText() == "X" && buttons[1].getText()  == "X" && buttons[2].getText()  == "X") {
+        if (buttons[0].getText().equals("X") && buttons[1].getText().equals("X")  && buttons[2].getText().equals("X")) {
             xWin(0, 1, 2);
-        } else if (buttons[3].getText() == "X" && buttons[4].getText()  == "X" && buttons[5].getText()  == "X") {
+        } else if (buttons[3].getText().equals("X") && buttons[4].getText().equals("X") && buttons[5].getText().equals("X")) {
             xWin(3, 4, 5);
-        } else if (buttons[6].getText() == "X" && buttons[7].getText()  == "X" && buttons[8].getText()  == "X") {
+        } else if (buttons[6].getText().equals("X") && buttons[7].getText().equals("X") && buttons[8].getText().equals("X")) {
             xWin(6, 7, 8);
         }
 
-        if (buttons[0].getText() == "O" && buttons[1].getText()  == "O" && buttons[2].getText()  == "O") {
-            xWin(0, 1, 2);
-        } else if (buttons[3].getText() == "O" && buttons[4].getText()  == "O" && buttons[5].getText()  == "O") {
-            xWin(3, 4, 5);
-        } else if (buttons[6].getText() == "O" && buttons[7].getText()  == "O" && buttons[8].getText()  == "O") {
-            xWin(6, 7, 8);
+        if (buttons[0].getText().equals("O") && buttons[1].getText().equals("O") && buttons[2].getText().equals("O")) {
+            oWin(0, 1, 2);
+        } else if (buttons[3].getText().equals("O") && buttons[4].getText().equals("O") && buttons[5].getText().equals("O")) {
+            oWin(3, 4, 5);
+        } else if (buttons[6].getText().equals("O") && buttons[7].getText().equals("O") && buttons[8].getText().equals("O")) {
+            oWin(6, 7, 8);
         }
 
 //        Vertical
-        if (buttons[0].getText() == "X" && buttons[3].getText()  == "X" && buttons[6].getText()  == "X") {
+        if (buttons[0].getText().equals("X") && buttons[3].getText().equals("X") && buttons[6].getText().equals("X")) {
             xWin(0, 3, 6);
-        } else if (buttons[1].getText() == "X" && buttons[4].getText()  == "X" && buttons[7].getText()  == "X") {
+        } else if (buttons[1].getText().equals("X") && buttons[4].getText().equals("X") && buttons[7].getText().equals("X")) {
             xWin(1, 4, 7);
-        } else if (buttons[2].getText() == "X" && buttons[5].getText()  == "X" && buttons[8].getText()  == "X") {
+        } else if (buttons[2].getText().equals("X") && buttons[5].getText().equals("X") && buttons[8].getText().equals("X")) {
             xWin(2, 5, 8);
         }
 
-        if (buttons[0].getText() == "O" && buttons[3].getText()  == "O" && buttons[6].getText()  == "O") {
-            xWin(0, 3, 6);
-        } else if (buttons[1].getText() == "O" && buttons[4].getText()  == "O" && buttons[7].getText()  == "O") {
-            xWin(1, 4, 7);
-        } else if (buttons[2].getText() == "O" && buttons[5].getText()  == "O" && buttons[8].getText()  == "O") {
-            xWin(2, 5, 8);
+        if (buttons[0].getText().equals("O") && buttons[3].getText().equals("O") && buttons[6].getText().equals("O")) {
+            oWin(0, 3, 6);
+        } else if (buttons[1].getText().equals("O") && buttons[4].getText().equals("O") && buttons[7].getText().equals("O")) {
+            oWin(1, 4, 7);
+        } else if (buttons[2].getText().equals("O") && buttons[5].getText().equals("O") && buttons[8].getText().equals("O")) {
+            oWin(2, 5, 8);
         }
 
 //        Diagonals
-        if (buttons[0].getText() == "X" && buttons[4].getText()  == "X" && buttons[8].getText()  == "X") {
+        if (buttons[0].getText().equals("X") && buttons[4].getText().equals("X") && buttons[8].getText().equals("X")) {
             xWin(0, 4, 8);
-        } else if (buttons[2].getText() == "X" && buttons[4].getText()  == "X" && buttons[6].getText()  == "X") {
+        } else if (buttons[2].getText().equals("X") && buttons[4].getText().equals("X") && buttons[6].getText().equals("X")) {
             xWin(2, 4, 6);
         }
 
-        if (buttons[0].getText() == "O" && buttons[4].getText()  == "O" && buttons[8].getText()  == "O") {
-            xWin(0, 4, 8);
-        } else if (buttons[2].getText() == "O" && buttons[4].getText()  == "O" && buttons[6].getText()  == "O") {
-            xWin(2, 4, 6);
+        if (buttons[0].getText().equals("O") && buttons[4].getText().equals("O") && buttons[8].getText().equals("O")) {
+            oWin(0, 4, 8);
+        } else if (buttons[2].getText().equals("O") && buttons[4].getText().equals("O") && buttons[6].getText().equals("O")) {
+            oWin(2, 4, 6);
         }
-
     }
 
-    // TODO: implement method to change winning combination buttons
-    //  to green and get the title to say x wins disable buttons
     public void xWin(int a, int b, int c) {
+        buttons[a].setForeground(Color.GREEN);
+        buttons[b].setForeground(Color.GREEN);
+        buttons[c].setForeground(Color.GREEN);
+
+        for (int i = 0; i < 9; i++) {
+            if (i == a || i == b || i == c) {
+                buttons[i].setEnabled(true);
+            } else {
+                buttons[i].setEnabled(false);
+            }
+        }
+        title.setText("X wins!");
+        xScore.setText("X: " + (++xPoints));
 
     }
 
-    // TODO: implement method to change winning combination buttons
-    //  to green and get the title to say o wins disable buttons
     public void oWin(int a, int b, int c) {
+        for (int i = 0; i < 9; i++) {
+            if (i == a || i == b || i == c) {
+                buttons[i].setEnabled(true);
+            } else {
+                buttons[i].setEnabled(false);
+            }
+        }
 
+        buttons[a].setForeground(Color.GREEN);
+        buttons[b].setForeground(Color.GREEN);
+        buttons[c].setForeground(Color.GREEN);
+        title.setText("O wins!");
+        oScore.setText("O: " + (++oPoints));
     }
 }
+
